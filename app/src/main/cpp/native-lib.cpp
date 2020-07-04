e3cbd74b0eaaf2a8202f869ae70142737951319a
Kartik Ohri <kartikohri13@gmail.com>
2020-07-05 01:38:09 +0530

    Add JNI interface.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include "streaming_extractor_music.h"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_org_metabrainz_essentiaandroid_MainActivity_stringFromJNI(
@@ -7,4 +8,17 @@ Java_org_metabrainz_essentiaandroid_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jint JNICALL
+Java_org_metabrainz_essentiaandroid_MainActivity_essentiaMusicExtractor(JNIEnv *env, jobject thiz,
+        jstring input_path, jstring output_path) {
+    jboolean is_copy;
+    const char *convertedInputPath = env->GetStringUTFChars(input_path, &is_copy);
+    const char *convertedOutputPath = env->GetStringUTFChars(output_path, &is_copy);
+    std::string input = std::string(convertedInputPath);
+    std::string output = std::string(convertedOutputPath);
+    int returnCode = essentia_main(input, output);
+    return returnCode;
 }
